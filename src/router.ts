126baf0cc14dda82dd96ee76f99e71681296ec8e
hamitsehjal <hamitsehjal26@gmail.com>
2022-12-13 15:36:58 -0500

    Route Validation - 'IN PROGRESS'

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
 import { Router } from "express"
 import { dirname } from "path";
-import { body, validationResult } from 'express-validator'
-import {handleInputValidation} from './modules/middleware'
+import { body, oneOf, validationResult } from 'express-validator'
+import { handleInputValidation } from './modules/middleware'
 import path from "path"
 const router = Router();
 
@@ -15,18 +15,17 @@ router.get("/product/:id", () => {
 
 })
 
-router.post("/product", () => {
+router.post("/product", body("name").isString(), handleInputValidation, (req, res) => {
+
+})
+
+router.put("/product/:id", body("name").isString(), handleInputValidation, (req, res) => {
 
 })
 
 router.delete("/product/:id", () => {
 
 })
-
-router.put("/product/:id", body("name").isString(),handleInputValidation,(req,res) => {
-
-})
-
 // UPDATE
 router.get("/update", () => {
 
@@ -36,13 +35,21 @@ router.get("/update/:id", () => {
 
 })
 
-router.post("/update", () => {
+router.post("/update",
+    body('title').exists().isString(),
+    body('body').exists().isString(),
+    () => {
 
-})
+    })
 
-router.put("/update/:id", () => {
+router.put("/update/:id",
+    body('title').optional(),
+    body('body').optional(),
+    body('status').isIn(['IN_PROGRESS','LIVE','DEPRECATED','ARCHIVED']),
+    body('version').optional(),
+    (req, res) => {
 
-})
+    })
 
 router.delete("/update/:id", () => {
 
@@ -53,17 +60,24 @@ router.get("/updatePoint", () => {
 
 })
 
-router.get("/updatePoint/:id", () => {
+router.get("/updatePoint/:id",
+    body('name').optional().exists(),
+    body('description').exists().isString(),
+    body('updateId').exists().isString(),
+    () => {
 
-})
+    })
 
 router.post("/updatePoint", () => {
 
 })
 
-router.put("/updatePoint/:id", () => {
+router.put("/updatePoint/:id",
+    body('name').optional().isString(),
+    body('description').optional().isString(),
+    () => {
 
-})
+    })
 
 router.delete("/updatePoint/:id", () => {
 
