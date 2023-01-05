6d4686e370be8de65f504b3e887f060826644c56
hamitsehjal <hamitsehjal26@gmail.com>
2023-01-05 02:37:09 -0500

    made route handlers for "update" schema

--- src/handlers/updates.ts
+++ src/handlers/updates.ts
@@ -23,8 +23,7 @@ export const getUpdates = async (req, res) => {
 export const getOneUpdate = async (req, res) => {
     const updated = await prisma.update.findFirst({
         where: {
-            id: req.params.id,
-            belongsTo: req.user.id
+            id: req.params.id
         }
     })
 
@@ -36,7 +35,7 @@ export const createUpdate = async (req, res) => {
 
     const product = await prisma.product.findUnique({
         where: {
-            id: req.body.id
+            id: req.body.productId
         }
     })
 
@@ -46,7 +45,18 @@ export const createUpdate = async (req, res) => {
     }
 
     const update = await prisma.update.create({
+
         data: req.body
+        // data:{
+        //     title:req.body.title,
+        //     body:req.body.body,
+        //     product:{
+        //         connect:{
+        //             id:product.id
+        //         }
+        //     }
+        // }
+
     })
 
     res.json({ data: update })
